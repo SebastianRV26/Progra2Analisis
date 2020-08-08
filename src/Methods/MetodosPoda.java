8d098556d1c337de9f514f705c8a74bd4d4808d7
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-08-08 10:56:51 -0600

    Ultimos cambios

--- src/Methods/MetodosPoda.java
+++ src/Methods/MetodosPoda.java
@@ -142,7 +142,6 @@ public class MetodosPoda {
      */
     public  void imprimirRuta(Poda temp){
          ArrayList<vertice> rutaVertices = temp.ruta;
-         System.out.println(rutaVertices);
          for (int i = 0; i < rutaVertices.size() - 1; i++) {
             vertice origen = rutaVertices.get(i);
             vertice destino = rutaVertices.get(i + 1);
